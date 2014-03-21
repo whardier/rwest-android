4fff8dfae736ce8b5cf15cdfcf7880bb2055e6e5
Shane Spencer <sspencer@abrinc.com>
2014-03-20 16:21:12 -0800

    my intentions feel funny

--- src/com/rwestful/android/MainActivity.java
+++ src/com/rwestful/android/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
@@ -63,7 +64,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 		setContentView(R.layout.activity_main);
 
 		context = this.getApplicationContext();
-
+		
 		// Set up the action bar.
 		final ActionBar actionBar = getActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
@@ -211,4 +212,12 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 		}
 	}
 
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+
+		android.util.Log.d("INTENT", this.getIntent().toString());
+
+	}
 }
